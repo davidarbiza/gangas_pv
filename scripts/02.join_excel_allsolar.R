aeaf6c950acaac62583f6c42528e6dc2c5c3e007
davidarbiza <david.arbiza@gmail.com>
2025-09-23 14:16:58 +0200

    Update_join_Spain_Excell_allsolar

--- scripts/02.join_excel_allsolar.R
+++ scripts/02.join_excel_allsolar.R
@@ -11,6 +11,15 @@ allsolar <- st_read("data/allsolar_cut.shp", quiet = TRUE)
 # Assure same CRS
 allsolar <- st_transform(allsolar, st_crs(pv_spain_excell))
 
+#Create id por each obs.
+pv_spain_excell <- pv_spain_excell %>%
+  mutate(point_id = row_number())
+
+#Buffer
+pv_spain_excell <- pv_spain_excell %>%
+  mutate(buffer_radius = sqrt(`Capacity..MW.`) * 50) %>%
+  st_buffer(dist = .$buffer_radius)
+
 # Spacial Join
 joined <- st_join(allsolar, pv_spain_excell, join = st_intersects)
 
@@ -19,13 +28,25 @@ joined_clean <- joined %>%
   clean_names()
 
 # Select relevant columns
-cols_to_keep <- c("project_name", "start_year", "capacity_mw")
+cols_to_keep <- c("point_id", "project_name", "start_year", "capacity_mw")
 joined_final <- joined_clean %>%
   select(all_of(cols_to_keep), geometry)
 
-# Verify geometry
-st_geometry(joined_final)
 
 # Save results
 st_write(joined_final, "data/allsolar_Spain_joined.gpkg", delete_layer = TRUE)
 st_write(joined_final, "data/allsolar_Spain_joined.shp", delete_layer = TRUE)
+
+#Check if the layer works
+joined <- st_read("data/allsolar_Spain_joined.gpkg", quiet = TRUE)
+st_geometry_type(joined) #Geometry
+st_crs(joined) #CRS
+sum(!is.na(joined_final$project_name)) #Polygons with data
+
+# Check overlaps
+poligonos_solapados <- joined_final %>%
+  group_by(geometry) %>% 
+  summarise(n_points = n_distinct(point_id)) %>%
+  filter(n_points > 1)
+nrow(poligonos_solapados)
+
